856892fcbaf808101de3fca69f88d6ed27b4a8f7
Zack Martin <zack@Zacks-Air.home>
2018-06-20 20:17:17 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintance.hbs', {
+        title: 'Maintance Page',
+        header: 'Site is under construction',
+        para: 'This site will be up and running soon, sorry for any inconvenience.'
+    });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+   return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeInfo: 'Thanks for taking the time to view this page'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Something went wrong'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+});
